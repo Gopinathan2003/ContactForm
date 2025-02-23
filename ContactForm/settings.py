51a349e237a2468eecb840961aedea943b91a3dd
GopinathanAsokan <gopinathanasokan591@gmail.com>
2025-02-23 22:30:06 +0530

    second

--- ContactForm/settings.py
+++ ContactForm/settings.py
@@ -117,8 +117,27 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG",
+    },
+}
--- cf/views.py
+++ cf/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from cf.forms import *
+import logging
 
 # Create your views here.
     
 def form(request):  
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        value = logging.getLogger("Test")
+        if form.is_valid():
+            value.debug(f'{form.cleaned_data['name']},{form.cleaned_data['email']},{form.cleaned_data['message']}')
+            success_message = "Your Email has been sent!"
+            return render(request, 'form.html', {'form':form, 'success_message':success_message})
+        else:
+            value.debug("Form is not valid")
+        return render(request, 'form.html', {'form': form, 'name': name, 'email': email, 'message': message, })
     return render(request, 'form.html')
